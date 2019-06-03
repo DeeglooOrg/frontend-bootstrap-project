8cd499c72fac036bbf40b531b672205ba58b945f
Luka Zrnić <luka.zrnic@deegloo.com>
2019-06-03 17:07:42 +0200

    Added example test

--- application/src/tests/HomePage.test.tsx
+++ application/src/tests/HomePage.test.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { shallow, configure } from 'enzyme';
+
+// the next two lines are needed to make the tests work (for React)
+import * as Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
 import HomePage from '../Components/HomePage/HomePage';
-// import { shallow } from 'enzyme';
 
-it('renders', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<HomePage />, div)
-})
+describe('<HomePage /> component', () => {
+  it('should render HomePage', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<HomePage />, div)
+  })
 
-
+  it('should contain "<h1>Home page</h1>" tag', () => {
+    const result = shallow(<HomePage />).contains(<h1>Home page</h1>);
+    expect(result).toBeTruthy();
+  });
+});
